a8d0146b6e06113e684c448d8b5cb2c21c979a5d
mihitapatel <101076659+mihitapatel@users.noreply.github.com>
2023-04-18 20:13:49 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAY_Wq3YRUH2BDeZL2G82XbiPGMaVXcu3g",
+    authDomain: "car-racing-game-d6f2e.firebaseapp.com",
+    databaseURL: "https://car-racing-game-d6f2e-default-rtdb.firebaseio.com",
+    projectId: "car-racing-game-d6f2e",
+    storageBucket: "car-racing-game-d6f2e.appspot.com",
+    messagingSenderId: "346995544578",
+    appId: "1:346995544578:web:2b40db150fc85ab799cd1b"
+  };
+  
